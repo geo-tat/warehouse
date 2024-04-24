7ecaef054659d6f5d47c7a398b5ee7e583ed89c9
Georgii Tatevosian <116814591+geo-tat@users.noreply.github.com>
2024-04-24 20:01:02 +0400

    Change @Data to @Getter/Setter (#5)

--- src/main/java/ru/mediasoft/warehouse/model/Product.java
+++ src/main/java/ru/mediasoft/warehouse/model/Product.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UuidGenerator;
 
@@ -17,7 +14,8 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Table(name = "products")
 @Builder
 @AllArgsConstructor
