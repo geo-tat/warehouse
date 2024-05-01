6bd74e37a9efa19952704c34cef6dd147bedab49
Georgii Tatevosian <116814591+geo-tat@users.noreply.github.com>
2024-05-01 16:37:35 +0400

    fix: made custom Error-response (#7)

--- src/main/java/ru/mediasoft/warehouse/exception/ErrorResponse.java
+++ src/main/java/ru/mediasoft/warehouse/exception/ErrorResponse.java
@@ -1,12 +1,22 @@
 package ru.mediasoft.warehouse.exception;
 
 import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Setter
 @Getter
 public class ErrorResponse {
-    private final String error;
+    private String exception;
+    private String sourceClass;
+    private String message;
+    private LocalDateTime timestamp;
 
-    public ErrorResponse(String error) {
-        this.error = error;
+    public ErrorResponse(String exception, String sourceClass, String message, LocalDateTime timestamp) {
+        this.exception = exception;
+        this.sourceClass = sourceClass;
+        this.message = message;
+        this.timestamp = timestamp;
     }
 }
