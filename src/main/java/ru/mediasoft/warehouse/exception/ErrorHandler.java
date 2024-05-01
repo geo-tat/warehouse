23ae04eda74b2d2fda91704ffec8d3a5615da0ac
Georgii <nosleep2night@yandex.ru>
2024-05-01 16:36:47 +0400

    fix: made custom Error-response

--- src/main/java/ru/mediasoft/warehouse/exception/ErrorHandler.java
+++ src/main/java/ru/mediasoft/warehouse/exception/ErrorHandler.java
@@ -4,15 +4,13 @@ import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @RestControllerAdvice
 @Slf4j
@@ -21,28 +19,29 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleEntityNotFoundException(final EntityNotFoundException e) {
         log.error("Пользователь с данным ID не найден.", e);
-        return new ErrorResponse(
-                e.getMessage());
+        return new ErrorResponse(e.getClass().getSimpleName(),
+                Arrays.stream(e.getStackTrace()).findFirst().toString(),
+                e.getMessage(),
+                LocalDateTime.now());
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleUniqueSkyException(final DataIntegrityViolationException e) {
         log.error("Товар с данным артикулом уже есть в базе данных", e);
-        return new ErrorResponse("Товар с данным артикулом уже есть в базе данных");
+        return new ErrorResponse(e.getClass().getSimpleName(),
+                Arrays.stream(e.getStackTrace()).findFirst().toString(),
+                "Товар с данным артикулом уже есть в базе данных",
+                LocalDateTime.now());
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public List<String> handleValidationException(MethodArgumentNotValidException ex) {
-        List<String> errors = new ArrayList<>();
-        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            if (error instanceof FieldError fieldError) {
-                errors.add(fieldError.getField() + ": " + fieldError.getDefaultMessage());
-            } else {
-                errors.add(error.getDefaultMessage());
-            }
-        }
-        return errors;
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+        log.error("Ошибка валидации сущности", e);
+        return new ErrorResponse(e.getClass().getSimpleName(),
+                Arrays.stream(e.getStackTrace()).findFirst().toString(),
+                e.getMessage(),
+                LocalDateTime.now());
     }
 }
