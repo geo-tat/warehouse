2a9695fc24dec35412103456c523f74aaaf42907
Georgii Tatevosian <116814591+geo-tat@users.noreply.github.com>
2024-06-12 13:07:22 +0300

    fix: currency package was refactored. (#17)

--- src/main/java/ru/mediasoft/warehouse/currency/CurrencyFilter.java
+++ src/main/java/ru/mediasoft/warehouse/currency/CurrencyFilter.java
@@ -1,4 +1,4 @@
-package ru.mediasoft.warehouse.product.service.currency;
+package ru.mediasoft.warehouse.currency;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
--- src/main/java/ru/mediasoft/warehouse/currency/CurrencyServiceClientImpl.java
+++ src/main/java/ru/mediasoft/warehouse/currency/CurrencyServiceClientImpl.java
@@ -1,4 +1,4 @@
-package ru.mediasoft.warehouse.product.service.currency;
+package ru.mediasoft.warehouse.currency;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
